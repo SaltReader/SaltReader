a419d88ba7a0d5861558dad1ed8b397e7d279a63
SaltReader <165895803+SaltReader@users.noreply.github.com>
2024-04-03 22:39:38 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: '/logo.png' }],
         ['script', { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5832122675149165" }],
+        ['meta', { name: 'google-adsense-account', content: 'ca-pub-5832122675149165' }],
       ],
       themeConfig: {
         logo: '/logo.png',
